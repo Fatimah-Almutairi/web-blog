126899e2aaef1ba31246d8ea23ec937773d864f5
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2022-12-06 08:37:27 +0300

    frontend

--- blog-frontend/src/pages/HomePage.tsx
+++ blog-frontend/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
-import { Card, CardBody, Flex, VStack , Text, Heading} from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react'
+import { Card, CardBody, Flex, VStack , Text, Heading, useToast} from '@chakra-ui/react';
+import { useEffect, useState } from 'react'
 
 function HomePage() {
-const [blog, setBlog] = useState();
-  useEffect( () => {
+const [blog, setBlog] = useState<string[]>([]);
+const [title, setTitle] = useState('');
+const toast = useToast();
+
     const fetchBlog = async () => {
       const request = await fetch('/api/v1/blog',{
          headers: {
@@ -13,15 +15,53 @@ const [blog, setBlog] = useState();
       const data = await request.json();
       setBlog(data);
     };
+    const addNew = async () => {
+      try{
+        if(!title) {
+          return;
+        }
+      
+      const request = await fetch('/api/v1/blog', {
+        method: 'POST',
+        headers:{ 'Content-Type': 'application/json',
+        Authorization: 'Bearer' + localStorage.getItem('token'),
+      },
+      body: JSON.stringify({title}),
+      });
+      const data = await request.json();
+
+      if(request.status !== 201){
+        toast({
+          title: data.message,
+          status: 'error',
+          duration: 3000,
+          position: 'top',
+        });
+        return;
+      }
+      fetchBlog();
+      setTitle('');
+    }catch(error){
+      console.log(error)
+      toast({
+        title: 'Server Error',
+        status: 'error',
+        duration: 3000,
+        position: 'top',
+      });
+    }
+    };
+    useEffect ( () => {
     fetchBlog();
-  },[])
+    },[])
+
   return (
   <Flex justifyContent='center' alignItems='center' height='100vh'>
       <Heading>Blogs</Heading>
       <VStack border='1rem' padding='3rem'>
         {blog.map( (blog: any) => (
       <Card>
-      <CardBody>
+      <CardBody key={blog.id}>
         <Text>{blog.title}</Text>
       </CardBody>
     </Card>
